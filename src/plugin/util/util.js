68a6e02d5aa9484033a558717d9d3b8296ba9b34
ouJiHong <1844478327@qq.com>
2017-09-26 11:16:33 +0800

    添加获取cookie的方法

--- src/plugin/util/util.js
+++ src/plugin/util/util.js
@@ -647,6 +647,38 @@ function showError(error){
         type:"error"
     });
 }
+
+// 添加Cookie
+function addCookie(name, value, options) {
+    if (arguments.length > 1 && name != null) {
+        if (options == null) {
+            options = {};
+        }
+        if (value == null) {
+            options.expires = -1;
+        }
+        if (typeof options.expires == "number") {
+            var time = options.expires;
+            var expires = options.expires = new Date();
+            expires.setTime(expires.getTime() + time * 1000);
+        }
+        document.cookie = encodeURIComponent(String(name)) + "=" + encodeURIComponent(String(value)) + (options.expires ? "; expires=" + options.expires.toUTCString() : "") + (options.path ? "; path=" + options.path : "") + (options.domain ? "; domain=" + options.domain : ""), (options.secure ? "; secure" : "");
+    }
+}
+
+// 获取Cookie
+function getCookie(name) {
+    if (name != null) {
+        var value = new RegExp("(?:^|; )" + encodeURIComponent(String(name)) + "=([^;]*)").exec(document.cookie);
+        return value ? decodeURIComponent(value[1]) : null;
+    }
+}
+
+// 移除Cookie
+function removeCookie(name, options) {
+    addCookie(name, null, options);
+}
+
 //工具对象
 var util = {
     showLoading:showLoading,
@@ -661,7 +693,9 @@ var util = {
     formData:formData,
     isEmpty:isEmpty,
     showError:showError,
-    additionErrorHandler:additionErrorHandler
+    additionErrorHandler:additionErrorHandler,
+    getCookie:getCookie,
+    removeCookie:removeCookie
 };
 
 export default  util;
